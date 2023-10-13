e455c83ef75d22640217ba10379a2f2348df8491
Julián Mora <j.morav@uniandes.edu.co>
2023-10-13 11:02:19 -0500

    central and remote scripts

---
+++ central_server.py
@@ -0,0 +1,74 @@
+import socket
+import json
+import psutil
+import threading
+import time
+
+# Variables para los datos del servidor central
+own_cpu_usage = 0.0
+own_ram_usage = 0.0
+own_disk_usage = 0.0
+
+# Datos de los computadores remotos (estos valores son solo de ejemplo)
+remote_client_data = {
+    "ID": "RemoteClient",
+    "CPUUsage": 42.0,
+    "RAMUsage": 58.7,
+    "DiskUsage": 23.1
+}
+
+# Identificador para el servidor central
+own_data = {
+    "ID": "ServerCentral",
+    "CPUUsage": 0.0,
+    "RAMUsage": 0.0,
+    "DiskUsage": 0.0
+}
+
+# Configuración para el servidor
+SERVER_IP = "157.253.192.187"  # Dirección IP del servidor (0.0.0.0 escucha en todas las interfaces)
+SERVER_PORT = 8000  # Puerto para la comunicación con los clientes
+BUFFER_SIZE = 1024
+
+# Función para enviar datos a un cliente
+def send_data_to_client(client_socket, data):
+    data_json = json.dumps(data)
+    client_socket.send(data_json.encode())
+
+# Función para manejar la conexión con un cliente
+def handle_client(client_socket):
+    global own_cpu_usage, own_ram_usage, own_disk_usage
+
+    while True:
+        # Recopila los datos propios del servidor
+        own_cpu_usage = psutil.cpu_percent(interval=1)
+        own_ram_usage = psutil.virtual_memory().percent
+        own_disk_usage = psutil.disk_usage("/").percent
+        own_data["CPUUsage"] = own_cpu_usage
+        own_data["RAMUsage"] = own_ram_usage
+        own_data["DiskUsage"] = own_disk_usage
+
+        # Envía los datos al cliente
+        send_data_to_client(client_socket, own_data)
+
+        # Espera un intervalo de tiempo antes de enviar nuevamente
+        time.sleep(5)
+
+# Función principal del servidor
+def run_server():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((SERVER_IP, SERVER_PORT))
+    server.listen(5)
+
+    print(f"Server started on {SERVER_IP}:{SERVER_PORT}")
+
+    while True:
+        client_socket, addr = server.accept()
+        print(f"Accepted connection from {addr}")
+
+        client_handler = threading.Thread(target=handle_client, args=(client_socket,))
+        client_handler.start()
+
+if __name__ == "__main__":
+    server_thread = threading.Thread(target=run_server)
+    server_thread.start()
---
+++ remote_computer.py
@@ -0,0 +1,57 @@
+import json
+import psutil
+import requests
+
+# Dirección del servidor central
+server_address = "http://157.253.192.187:8000"
+
+# En este ejemplo, el servidor central se identifica con ID 0
+server_id = 0
+
+# Asignación de ID: el servidor central asigna IDs únicos a los clientes remotos
+def assign_client_id():
+    data = {"assign_id": True}
+    response = requests.get(f"{server_address}/assign_id")
+    if response.status_code == 200:
+        return response.json()["client_id"]
+    else:
+        print("Failed to assign client ID.")
+        return None
+
+# Envío de datos al servidor central
+def send_data_to_server(data, client_id):
+    url = f"{server_address}/data"
+    headers = {'Content-Type': 'application/json'}
+
+    data["ID"] = client_id
+
+    # Enviar datos como JSON al servidor central
+    response = requests.post(url, data=json.dumps(data), headers=headers)
+
+    if response.status_code == 200:
+        print("Data sent successfully")
+    else:
+        print(f"Failed to send data. Status code: {response.status_code}")
+
+# Recopilación y envío de datos
+def collect_data(client_id):
+    cpu_usage = psutil.cpu_percent(interval=1)
+    ram_usage = psutil.virtual_memory().percent
+    disk_usage = psutil.disk_usage("/").percent
+
+    data = {
+        "CPUUsage": cpu_usage,
+        "RAMUsage": ram_usage,
+        "DiskUsage": disk_usage
+    }
+
+    send_data_to_server(data, client_id)
+
+if __name__ == "__main__":
+    # Asignar un ID único al cliente remoto
+    client_id = assign_client_id()
+
+    if client_id is not None:
+        # Iniciar la recopilación y el envío de datos
+        while True:
+            collect_data(client_id)
