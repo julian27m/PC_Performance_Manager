54172cddfb46215aa319a5a873cd4de60f6e7bad
Julián Mora <j.morav@uniandes.edu.co>
2023-10-13 15:32:27 -0500

    Sockets implementation

--- central_server.py
+++ central_server.py
@@ -1,25 +1,48 @@
-from flask import Flask, request, jsonify
+import socket
+import json
+import threading
 
-app = Flask(__name__)
+# Dirección y puerto del servidor central
+server_ip = "0.0.0.0"  # Escucha en todas las interfaces
+server_port = 8000
 
-# Datos recopilados del servidor central
-own_data = {
-    "ID": "ServerCentral",
-    "CPUUsage": 0.0,
-    "RAMUsage": 0.0,
-    "DiskUsage": 0.0
-}
+# Lista para almacenar las conexiones de los clientes remotos
+client_connections = []
 
-# Ruta para recibir datos del cliente remoto
-@app.route("/data", methods=["POST"])
-def receive_data():
-    data = request.get_json()
-    print("Received data from client:", data)
+# Función para manejar las conexiones de los clientes remotos
+def handle_client(client_socket):
+    try:
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break
 
-    # Realiza cualquier procesamiento adicional si es necesario
-    # ...
+            # Decodificar los datos recibidos (asumimos que son JSON)
+            received_data = json.loads(data.decode())
 
-    return "Data received successfully", 200
+            # Aquí puedes procesar los datos según tus necesidades
+            print("Received data:", received_data)
+
+    except Exception as e:
+        print(f"Error handling client connection: {e}")
+    finally:
+        client_socket.close()
+
+# Función principal del servidor
+def run_server():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((server_ip, server_port))
+    server.listen(5)
+
+    print(f"Server started on {server_ip}:{server_port}")
+
+    while True:
+        client_socket, addr = server.accept()
+        print(f"Accepted connection from {addr}")
+
+        client_connections.append(client_socket)
+        client_handler = threading.Thread(target=handle_client, args=(client_socket,))
+        client_handler.start()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    run_server()
--- remote_computer.py
+++ remote_computer.py
@@ -1,60 +1,41 @@
+import socket
 import json
 import psutil
-import requests
+import time
 
 # Dirección del servidor central
-server_address = "http://157.253.192.187:8000"
+server_address = "157.253.192.187"  
+server_port = 8000
 
-# En este ejemplo, el servidor central se identifica con ID 0
-server_id = 0
-
-# Asignación de ID: el servidor central asigna IDs únicos a los clientes remotos
-def assign_client_id():
-    data = {"assign_id": True}
-    response = requests.get(f"{server_address}/assign_id")
-    if response.status_code == 200:
-        client_id = response.json()["client_id"]
-        print("Client ID assigned:", client_id)
-        return client_id
-    else:
-        print("Failed to assign client ID.")
-        return None
-
-# Envío de datos al servidor central
-def send_data_to_server(data, client_id):
-    url = f"{server_address}/data"
-    headers = {'Content-Type': 'application/json'}
-
+# Función para enviar datos al servidor central
+def send_data_to_server(data, client_id, server_socket):
     data["ID"] = client_id
+    data_json = json.dumps(data)
+    server_socket.send(data_json.encode())
 
-    # Enviar datos como JSON al servidor central
-    response = requests.post(url, data=json.dumps(data), headers=headers)
-
-    if response.status_code == 200:
-        print("Data sent successfully")
-    else:
-        print(f"Failed to send data. Status code: {response.status_code}")
-
-# Recopilación y envío de datos
-def collect_data(client_id):
-    cpu_usage = psutil.cpu_percent(interval=1)
-    ram_usage = psutil.virtual_memory().percent
-    disk_usage = psutil.disk_usage("/").percent
-
-    data = {
-        "CPUUsage": cpu_usage,
-        "RAMUsage": ram_usage,
-        "DiskUsage": disk_usage
-    }
-
-    print("Collecting data:", data)
-    send_data_to_server(data, client_id)
+# Función para obtener el ID único del cliente remoto (si es necesario)
+def assign_client_id(server_socket):
+    # Implementa esta función si deseas asignar un ID al cliente desde el servidor central
+    pass
 
 if __name__ == "__main__":
-    # Asignar un ID único al cliente remoto
-    client_id = assign_client_id()
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((server_address, server_port))
+    client_id = assign_client_id(client_socket)
 
-    if client_id is not None:
-        # Iniciar la recopilación y el envío de datos
-        while True:
-            collect_data(client_id)
+    while True:
+        # Recopila los datos en este loop
+        cpu_usage = psutil.cpu_percent(interval=1)
+        ram_usage = psutil.virtual_memory().percent
+        disk_usage = psutil.disk_usage("/").percent
+
+        data = {
+            "CPUUsage": cpu_usage,
+            "RAMUsage": ram_usage,
+            "DiskUsage": disk_usage
+        }
+
+        # Envía los datos al servidor central
+        send_data_to_server(data, client_id, client_socket)
+
+        time.sleep(5)  # Ajusta la frecuencia de envío según sea necesario
