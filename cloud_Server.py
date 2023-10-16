c650ea1bab8b5f2b55c62e81f4dfff8737b2c6fa
Julián Mora <j.morav@uniandes.edu.co>
2023-10-16 13:21:31 -0500

    revert server changes

--- cloud_Server.py
+++ cloud_Server.py
@@ -9,36 +9,23 @@ app = Flask(__name__)
 aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
 aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
-# Estructura de datos para almacenar información dinámica
-dynamic_data = {}
-
 # Configura el acceso a AWS S3
 s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
 
-@app.route('/data/<computer_id>', methods=['GET', 'POST'])
+@app.route('/data/<computer_id>', methods=['POST'])
 def receive_data(computer_id):
-    if request.method == 'GET':
-        # Aquí debes obtener los datos dinámicos de la computadora con ID específico
-        if computer_id in dynamic_data:
-            data = dynamic_data[computer_id]
-            return jsonify(data)
-        else:
-            return 'No se encontraron datos para la computadora', 404
+    try:
+        data = request.json
+        print(f"Datos recibidos de la computadora {computer_id}: {data}")  # Mensaje de depuración
 
-    if request.method == 'POST':
-        try:
-            data = request.json
+        file_name = f'{computer_id}.json'
+        s3.put_object(Bucket='colivri-computer-stats', Key=file_name, Body=json.dumps(data))
 
-            file_name = f'{computer_id}.json'
-            s3.put_object(Bucket='nombre-del-bucket', Key=file_name, Body=json.dumps(data))
-
-            # Almacena la información dinámica para la computadora
-            dynamic_data[computer_id] = data
-
-            return f"Datos de la computadora {computer_id} recibidos y almacenados correctamente", 200
-        except Exception as e:
-            return str(e), 400
-
+        return f"Datos de la computadora {computer_id} recibidos y almacenados correctamente", 200
+    except Exception as e:
+        print(f"Error al procesar datos de la computadora {computer_id}: {e}")  # Mensaje de depuración
+        return str(e), 400
+    
 @app.route('/')
 def index():
     return '¡Bienvenido a mi servidor en la nube!'
