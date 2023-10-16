c9879f3e202cf1e290896c5be8773629ebc0e532
Julián Mora <j.morav@uniandes.edu.co>
2023-10-16 13:18:38 -0500

    server GET updates

--- cloud_Server.py
+++ cloud_Server.py
@@ -3,29 +3,42 @@ import boto3
 import json
 import os
 
-app = Flask(__name__)
+app = Flask(__name)
 
 # Utiliza os.environ para acceder a las variables de entorno
 aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
 aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
+# Estructura de datos para almacenar información dinámica
+dynamic_data = {}
+
 # Configura el acceso a AWS S3
 s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
 
-@app.route('/data/<computer_id>', methods=['POST'])
+@app.route('/data/<computer_id>', methods=['GET', 'POST'])
 def receive_data(computer_id):
-    try:
-        data = request.json
-        print(f"Datos recibidos de la computadora {computer_id}: {data}")  # Mensaje de depuración
+    if request.method == 'GET':
+        # Aquí debes obtener los datos dinámicos de la computadora con ID específico
+        if computer_id in dynamic_data:
+            data = dynamic_data[computer_id]
+            return jsonify(data)
+        else:
+            return 'No se encontraron datos para la computadora', 404
 
-        file_name = f'{computer_id}.json'
-        s3.put_object(Bucket='colivri-computer-stats', Key=file_name, Body=json.dumps(data))
+    if request.method == 'POST':
+        try:
+            data = request.json
 
-        return f"Datos de la computadora {computer_id} recibidos y almacenados correctamente", 200
-    except Exception as e:
-        print(f"Error al procesar datos de la computadora {computer_id}: {e}")  # Mensaje de depuración
-        return str(e), 400
-    
+            file_name = f'{computer_id}.json'
+            s3.put_object(Bucket='nombre-del-bucket', Key=file_name, Body=json.dumps(data))
+
+            # Almacena la información dinámica para la computadora
+            dynamic_data[computer_id] = data
+
+            return f"Datos de la computadora {computer_id} recibidos y almacenados correctamente", 200
+        except Exception as e:
+            return str(e), 400
+
 @app.route('/')
 def index():
     return '¡Bienvenido a mi servidor en la nube!'
