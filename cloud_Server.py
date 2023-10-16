cb95b0b8b3e6fb612beef5dc2cfd33e8c9bd2d09
Julián Mora <j.morav@uniandes.edu.co>
2023-10-16 13:30:04 -0500

    app routing id:1

--- cloud_Server.py
+++ cloud_Server.py
@@ -12,16 +12,23 @@ aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
 # Configura el acceso a AWS S3
 s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
 
-@app.route('/data/<computer_id>', methods=['POST'])
+@app.route('/data/<computer_id>', methods=['GET', 'POST'])
 def receive_data(computer_id):
     try:
-        data = request.json
-        print(f"Datos recibidos de la computadora {computer_id}: {data}")  # Mensaje de depuración
+        if request.method == 'GET':
+            # Devuelve los datos de la computadora con el ID especificado
+            file_name = f'{computer_id}.json'
+            data = s3.get_object(Bucket='colivri-computer-stats', Key=file_name)['Body'].read().decode()
+            return jsonify(data), 200
+        else:
+            # Procesa los datos recibidos de la computadora
+            data = request.json
+            print(f"Datos recibidos de la computadora {computer_id}: {data}")  # Mensaje de depuración
 
-        file_name = f'{computer_id}.json'
-        s3.put_object(Bucket='colivri-computer-stats', Key=file_name, Body=json.dumps(data))
+            file_name = f'{computer_id}.json'
+            s3.put_object(Bucket='colivri-computer-stats', Key=file_name, Body=json.dumps(data))
 
-        return f"Datos de la computadora {computer_id} recibidos y almacenados correctamente", 200
+            return f"Datos de la computadora {computer_id} recibidos y almacenados correctamente", 200
     except Exception as e:
         print(f"Error al procesar datos de la computadora {computer_id}: {e}")  # Mensaje de depuración
         return str(e), 400
@@ -29,6 +36,7 @@ def receive_data(computer_id):
 @app.route('/')
 def index():
     return '¡Bienvenido a mi servidor en la nube!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
